645824e191d1d3e1e14d36c0ec1102ae85d68af8
Nguyen Quang Duc <nguyenquangduc2000@gmail.com>
2020-04-18 15:48:34 +0700

    Update model and mapping

--- model.py
+++ model.py
@@ -6,11 +6,16 @@ from keras.callbacks import ModelCheckpoint, EarlyStopping
 from sequence_generator import SequenceGenerator
 from keras.utils import multi_gpu_model
 from text_utils import *
+import h5py
+import numpy as np
 
 def build_model(vocab_size, seq_length=30, batch_size=32):
     model = Sequential()
 
     model.add(Embedding(vocab_size, 200, input_length=seq_length, trainable=True))
+    model.add(LSTM(512, return_sequences=True))
+    model.add(LSTM(512, return_sequences=True))
+    model.add(Dropout(0.15))
     model.add(LSTM(512, return_sequences=True))
     model.add(LSTM(512, return_sequences=True))
     model.add(Dropout(0.15))
@@ -24,6 +29,59 @@ def build_model(vocab_size, seq_length=30, batch_size=32):
 
     return model
 
+def load_multigpu_checkpoint_weights(model, h5py_file):
+    """
+    Loads the weights of a weight checkpoint from a multi-gpu
+    keras model.
+
+    Input:
+
+        model - keras model to load weights into
+
+        h5py_file - path to the h5py weights file
+
+    Output:
+        None
+    """
+
+    print("Setting weights...")
+    with h5py.File(h5py_file, "r") as file:
+
+        # Get model subset in file - other layers are empty
+        weight_file = file["sequential_1"]
+
+        for layer in model.layers:
+
+            try:
+                layer_weights = weight_file[layer.name]
+                print('Loading %s layer...' % layer.name)
+            except:
+                # No weights saved for layer
+                continue
+
+            try:
+                weights = []
+                # Extract weights
+                for term in layer_weights:
+                    if isinstance(layer_weights[term], h5py.Dataset):
+                        # Convert weights to numpy array and prepend to list
+                        if layer.name == 'lstm_1':
+                            if term == 'bias:0':
+                                weights.insert(0, np.array(layer_weights[term]))
+                            elif term =='kernel:0':
+                                weights.insert(0, np.array(layer_weights[term]))
+                            else:
+                                weights.insert(1, np.array(layer_weights[term]))
+                        else:
+                            weights.insert(0, np.array(layer_weights[term]))
+
+                # Load weights to model
+                layer.set_weights(weights)
+
+            except Exception as e:
+                print(e)
+                print("Error: Could not load weights for layer: " + layer.name)
+
 class GenericLM():
     def __init__(self, vocab_size, mapping, seq_length=30, batch_size=32, multi_gpu=False,
                 ckpt_path='./ckpt', model_path='./model', mode_name='left2right'):
@@ -36,6 +94,7 @@ class GenericLM():
         self.mode_name = mode_name
 
         if os.path.exists(os.path.join(self.model_path, 'GenericLM_%s.model'%self.mode_name)):
+            print("Loading saved model...")
             self.model = load_model(os.path.join(self.model_path, 'GenericLM_%s.model'%self.mode_name))
         else:
             self.model = build_model(self.vocab_size, self.seq_length, self.batch_size)
@@ -66,7 +125,9 @@ class GenericLM():
         ckpt_file = os.listdir(self.ckpt_path)
         ckpt_file = list(filter(lambda x: x[-2:] == 'h5', ckpt_file))
         if ckpt_file:
+            print("Restoring model from checkpoint...")
             self.model.load_weights(os.path.join(self.ckpt_path, ckpt_file[-1]))
+            # load_multigpu_checkpoint_weights(self.model, os.path.join(self.ckpt_path, ckpt_file[-1]))
 
     def predict(self, X, return_prob_table=False, return_label=True):
         if return_prob_table:
