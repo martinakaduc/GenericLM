da948db2a5ef8d9056148f4f1a49f4c844126cd5
Nguyen Quang Duc <nguyenquangduc2000@gmail.com>
2020-06-18 10:10:52 +0700

    Revert model & disable prob

--- run.py
+++ run.py
@@ -23,14 +23,14 @@ def main(args):
             next_char = generic_lm.predict(encoded_text, return_prob_table=False, return_label=True)[0]
             input_text += next_char
 
-        sentence_prob = 1
-        for i, char in enumerate(input_text):
-            encoded_text = encode_sequence(mapping, input_text[:i].lower(), padding=True, seq_length=args.seq_length)
-            prob_table = generic_lm.predict(encoded_text, return_prob_table=True)[0]
-            cur_idx = mapping[char]
-            sentence_prob *= prob_table[cur_idx]
+        # sentence_prob = 1
+        # for i, char in enumerate(input_text):
+        #     encoded_text = encode_sequence(mapping, input_text[:i].lower(), padding=True, seq_length=args.seq_length)
+        #     prob_table = generic_lm.predict(encoded_text, return_prob_table=True)[0]
+        #     cur_idx = mapping[char.lower()]
+        #     sentence_prob *= prob_table[cur_idx]
 
-        print("Sentence probability: %.10f" % sentence_prob)
+        # print("Sentence probability: %.10f" % sentence_prob)
 
         if (args.mode == 'right2left'):
             input_text = input_text[::-1]
